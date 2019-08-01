0b5f2f0c2afeedd2d69200f38d1d2e22e90bec48
Nightinga1e <SMaximD@gmail.com>
2019-08-01 10:32:33 +0700

    Add fillfunc function

--- test.c
+++ test.c
@@ -4,18 +4,21 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int main() {
+
+int fillfun() {
 	int fd, f, tmp = 0;
 	char string[] = {"Hello World!"};
 	char buffer[14];
-	if(f = open ("test.txt", O_CREAT) == -1) printf("open error!");
+	int mode =0;
+	mode = mode |  S_IRUSR | S_IWUSR; 
+	if((f = open ("test.txt", O_CREAT , mode)) == -1) printf("open error!");
 
 	#ifdef TEST
 	f = open("text.txt", O_CREAT);
 	#endif
 
 	close(f);
-	if (fd = open("test.txt", O_WRONLY) == -1) printf("write error!");
+	if ((fd = open("test.txt", O_WRONLY)) == -1) printf("write error!");
 
 	#ifdef TEST
 	fd = open("test.txt" , O_WRONLY);
@@ -25,7 +28,7 @@ int main() {
 	write (fd, string, 12);
 	close(fd);
 
-	if(fd = open("test.txt", O_RDONLY) == -1) printf("read error!");
+	if((fd = open("test.txt", O_RDONLY)) == -1) printf("read error!");
 
 	for (int i = 1; i <= 12; i++){
 		lseek(fd, -i, SEEK_END);
@@ -33,5 +36,12 @@ int main() {
 		printf("%c", buffer[0]);
 	}
 	printf("\n");
+
 	return 0;
+}
+
+int main()
+{
+fillfun();
+return 0;
 }
